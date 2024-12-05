002efede750a5529b5f4382d20b365dbbedcec02
tandisSh <tandis00shojaee@gmail.com>
2024-12-05 12:47:31 +0330

    tables migration

--- database/migrations/2024_12_05_091024_create_articals_table.php
+++ database/migrations/2024_12_05_091024_create_articals_table.php
@@ -11,15 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('articals', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->tinyInteger('role')->default(0);
+            $table->string('title');
+            $table->string('master');
+            $table->text('body');
+            $table->text('image');
             $table->timestamps();
         });
-
     }
 
     /**
@@ -27,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('articals');
     }
 };
